6f1934689dd1da8f27be5c8797c16a54ae608994
Maximilian Weller <Maximilian Weller@DESKTOP-90RIJAV>
2017-05-15 15:30:41 +0200

    Customer Creation New eingefügt

--- Vidly/Controllers/CustomerController.cs
+++ Vidly/Controllers/CustomerController.cs
@@ -45,6 +45,11 @@ namespace Vidly.Controllers
             return View(customers);
         }
 
+        public ActionResult New()
+        {
+            return View();
+        }
+
 
         public ActionResult Details(int id)
         {
